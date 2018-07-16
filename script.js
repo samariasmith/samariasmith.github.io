c6a567fff787f620de78b216a86257a6a8ab9a02
samariasmith <41303276+samariasmith@users.noreply.github.com>
2018-07-16 15:14:37 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+alert("Welcome to my website");
+
+function clickFunction() {
+
+  document.getElementById("title").style.fontSize = "100px";
+  document.getElementById('theImage').src="noname2.webp";
+  document.getElementById('nailpic').src="nails.jpg";
+
+}
+
+function clickback()
+    {
+  document.getElementById("theImage").src="smino.jpg";
+  document.getElementById("nailpic").src="nailsss.jpg"
+    }
